4aa98d2010d6da91e99b27f3268a46c8969ce88d
Benson Maruchu <benmaruchu@gmail.com>
2018-05-15 13:34:13 +0300

    fix issue with search with option get specs

--- test/integration/service.get.spec.js
+++ test/integration/service.get.spec.js
@@ -73,10 +73,10 @@ describe('Service', function () {
           };
         });
       async
-      .parallel(fakes, function (error, created) {
-        services = created;
-        done(error, created);
-      });
+        .parallel(fakes, function (error, created) {
+          services = created;
+          done(error, created);
+        });
     });
 
     it('should be able to get without options', function (done) {
@@ -141,9 +141,9 @@ describe('Service', function () {
           expect(error).to.not.exist;
           expect(results).to.exist;
           expect(results.data).to.exist;
-          expect(results.data).to.have.length(1);
+          expect(results.data).to.have.length.of.at.least(1);
           expect(results.total).to.exist;
-          expect(results.total).to.be.equal(1);
+          expect(results.total).to.be.at.least(1);
           expect(results.limit).to.exist;
           expect(results.limit).to.be.equal(10);
           expect(results.skip).to.exist;
@@ -168,9 +168,9 @@ describe('Service', function () {
           expect(error).to.not.exist;
           expect(results).to.exist;
           expect(results.data).to.exist;
-          expect(results.data).to.have.length(1);
+          expect(results.data).to.have.length.of.at.least(1);
           expect(results.total).to.exist;
-          expect(results.total).to.be.equal(1);
+          expect(results.total).to.be.at.least(1);
           expect(results.limit).to.exist;
           expect(results.limit).to.be.equal(10);
           expect(results.skip).to.exist;
