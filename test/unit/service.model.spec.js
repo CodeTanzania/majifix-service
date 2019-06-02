d81b59cef26ef76b791516157a422bc6e385b356
rijkerd <richardaggrey7@gmail.com>
2019-06-02 20:19:50 +0300

    chore(unit): implement to es6 and fix prettier errors

--- test/unit/service.model.spec.js
+++ test/unit/service.model.spec.js
@@ -1,20 +1,11 @@
-'use strict';
-
-
 /* dependencies */
-const path = require('path');
-const { expect } = require('chai');
-
+import { expect } from 'chai';
 
 /* declarations */
-const Service =
-  require(path.join(__dirname, '..', '..', 'lib', 'service.model'));
-
+import Service from '../../src/service.model';
 
 describe('Service', () => {
-
   describe('Statics', () => {
-
     it('should expose model name as constant', () => {
       expect(Service.MODEL_NAME).to.exist;
       expect(Service.MODEL_NAME).to.be.equal('Service');
@@ -22,42 +13,31 @@ describe('Service', () => {
 
     it('should expose autopulate as options', () => {
       expect(Service.OPTION_AUTOPOPULATE).to.exist;
-      expect(Service.OPTION_AUTOPOPULATE)
-        .to.be.eql({
-          select: {
-            jurisdiction: 1,
-            group: 1,
-            priority: 1,
-            code: 1,
-            name: 1,
-            color: 1
-          },
-          maxDepth: 1
-        });
+      expect(Service.OPTION_AUTOPOPULATE).to.be.eql({
+        select: {
+          jurisdiction: 1,
+          group: 1,
+          priority: 1,
+          code: 1,
+          name: 1,
+          color: 1,
+        },
+        maxDepth: 1,
+      });
     });
-
   });
-
 
   describe('open311', () => {
+    it('should be able to convert service to open311 format', () => {
+      const service = Service.fake();
+      const object = service.toOpen311();
 
-    it('should be able to convert service to open311 format',
-      () => {
-        const service = Service.fake();
-        const object = service.toOpen311();
-
-        /* jshint camelcase:false*/
-        expect(object.service_code).to.exist;
-        expect(object.service_name).to.exist;
-        expect(object.description).to.exist;
-        expect(object.metadata).to.exist;
-        expect(object.type).to.exist;
-        expect(object.keywords).to.exist;
-
-        /* jshint camelcase:true*/
-
-      });
-
+      expect(object.service_code).to.exist;
+      expect(object.service_name).to.exist;
+      expect(object.description).to.exist;
+      expect(object.metadata).to.exist;
+      expect(object.type).to.exist;
+      expect(object.keywords).to.exist;
+    });
   });
-
 });
