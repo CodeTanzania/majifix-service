89d46ca50caaab92943dfe4e65a32cebdf49b55c
rijkerd <richardaggrey7@gmail.com>
2019-06-02 20:08:21 +0300

    chore: implement to es6

--- src/sla.schema.js
+++ src/sla.schema.js
@@ -1,6 +1,3 @@
-'use strict';
-
-
 /**
  * @module sla
  * @name sla
@@ -15,48 +12,45 @@
  * @version 0.1.0
  */
 
-
 /* @todo years, months, days, hours */
 
-
 /* dependencies */
-const { Schema } = require('mongoose');
-const { schema } = require('@codetanzania/majifix-common');
-
+import { Schema } from 'mongoose';
+import { schema } from '@codetanzania/majifix-common';
 
 /* declarations */
 const { SUB_DOC_SCHEMA_OPTIONS } = schema;
-
 
 /**
  * @name SlaSchema
  * @type {Schema}
  * @private
  */
-const SlaSchema = new Schema({
-  /**
-   * @name ttr
-   * @description time required in hours to resolve(mark as done)
-   * a service request(issue)
-   *
-   * @type {object}
-   * @property {object} type - schema(data) type
-   * @property {boolean} index - ensure database index
-   * @property {boolean} default - default value set when none provided
-   * @property {object} fake - fake data generator options
-   * @since 0.1.0
-   * @version 0.1.0
-   * @instance
-   */
-  ttr: {
-    type: Number,
-    index: true,
-    default: 0,
-    fake: true
-  }
-
-}, SUB_DOC_SCHEMA_OPTIONS);
-
+const SlaSchema = new Schema(
+  {
+    /**
+     * @name ttr
+     * @description time required in hours to resolve(mark as done)
+     * a service request(issue)
+     *
+     * @type {object}
+     * @property {object} type - schema(data) type
+     * @property {boolean} index - ensure database index
+     * @property {boolean} default - default value set when none provided
+     * @property {object} fake - fake data generator options
+     * @since 0.1.0
+     * @version 0.1.0
+     * @instance
+     */
+    ttr: {
+      type: Number,
+      index: true,
+      default: 0,
+      fake: true,
+    },
+  },
+  SUB_DOC_SCHEMA_OPTIONS
+);
 
 /* exports sla schema */
-module.exports = SlaSchema;
+export default SlaSchema;
