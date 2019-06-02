08edcf516dab3766053d88855f19fb05d11e950d
rijkerd <richardaggrey7@gmail.com>
2019-06-02 20:08:48 +0300

    chore(plugin): implement to es6

--- src/open311.plugin.js
+++ src/open311.plugin.js
@@ -1,6 +1,3 @@
-'use strict';
-
-
 /**
  * @module open311
  * @name open311
@@ -14,15 +11,12 @@
  * @version 0.1.0
  */
 
-
 /* dependencies */
-const _ = require('lodash');
-const { getString } = require('@lykmapipo/env');
-
+import _ from 'lodash';
+import { getString } from '@lykmapipo/env';
 
 /* constants */
-const DEFAULT_LOCALE = getString('DEFAULT_LOCALE','en');
-
+const DEFAULT_LOCALE = getString('DEFAULT_LOCALE', 'en');
 
 /**
  * @name unlocalize
@@ -36,39 +30,32 @@ const DEFAULT_LOCALE = getString('DEFAULT_LOCALE','en');
  * @private
  */
 function unlocalize(path, data) {
-
   // prepare unlocalized data
-  let unlocalized = {};
+  const unlocalized = {};
 
-  //prepare localized
-  let localized =
-    (data && _.isFunction(data.toObject) ? data.toObject() : data);
+  // prepare localized
+  let localized = data && _.isFunction(data.toObject) ? data.toObject() : data;
   localized = _.merge({}, localized);
 
-  _.forEach(localized, function (value, locale) {
-
-    //handle default locale
+  _.forEach(localized, function cb(value, locale) {
+    // handle default locale
     if (locale === DEFAULT_LOCALE) {
       unlocalized[path] = value;
     }
 
-    //handle other locales
+    // handle other locales
     else {
       const key = `${path}_${locale}`;
       unlocalized[key] = value;
     }
-
   });
 
-  //return unlocalized object
+  // return unlocalized object
   return _.omitBy(unlocalized, _.isUndefined);
-
 }
 
-
 /* implementation */
-module.exports = function open311Plugin(schema) {
-
+export default function open311Plugin(schema) {
   /**
    * @name toOpen311
    * @description convert service instance to Open311 compliant schema
@@ -79,10 +66,8 @@ module.exports = function open311Plugin(schema) {
    * @type {function}
    * @instance
    */
-  schema.methods.toOpen311 = function () {
-
-    /*jshint camelcase:false*/
-
+  // eslint-disable-next-line no-param-reassign
+  schema.methods.toOpen311 = function toOpen311() {
     let object = {};
 
     // The unique identifier for the service request type
@@ -96,8 +81,8 @@ module.exports = function open311Plugin(schema) {
     const description = unlocalize('description', this.description);
     object = _.merge({}, object, description);
 
-    //Determines whether there are additional form fields for this service type.
-    //Current we don't support additional form fields
+    // Determines whether there are additional form fields for this service type.
+    // Current we don't support additional form fields
     object.metadata = false;
 
     // The service request ID(ticket number) will be returned immediately after
@@ -113,12 +98,12 @@ module.exports = function open311Plugin(schema) {
     // A comma separated list of tags or keywords to help users identify
     // the request type. This can provide synonyms of the service_name and group.
     object.keywords = [].concat(_.values(name)).concat(_.values(group));
-    object.keywords = _.chain(object.keywords).compact().uniq().join(',').value();
-
-    /*jshint camelcase:true*/
+    object.keywords = _.chain(object.keywords)
+      .compact()
+      .uniq()
+      .join(',')
+      .value();
 
     return object;
-
   };
-
-};
+}
