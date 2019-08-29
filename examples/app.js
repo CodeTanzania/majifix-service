cae34a8d9b4e5b21471fa0ca35334965997712d1
lykmapipo <lallyelias87@gmail.com>
2019-08-29 21:27:00 +0300

    chore(examples): improve sample app & seed

--- examples/app.js
+++ examples/app.js
@@ -1,28 +1,35 @@
-// dependencies
-const app = require('@lykmapipo/express-common');
+const { get, mount, start } = require('@lykmapipo/express-common');
 const { connect, jsonSchema } = require('@lykmapipo/mongoose-common');
-const { router, info, apiVersion } = require('../lib/index');
+const { serviceRouter, info, apiVersion } = require('../lib/index');
 
-app.mount(router);
-
-connect(connectionError => {
-  if (connectionError) {
-    throw connectionError;
-  }
-
-  app.get('/', (request, response) => {
+const startHttpServer = () => {
+  get('/', (request, response) => {
     response.status(200);
     response.json(info);
   });
 
-  app.get(`/${apiVersion}/schemas`, (request, response) => {
+  get(`/${apiVersion}/schemas`, (request, response) => {
     const schema = jsonSchema();
     response.status(200);
     response.json(schema);
   });
 
-  /* fire the app */
-  app.start((error, env) => {
+  // mount routers
+  mount(serviceRouter);
+
+  // fire http serve
+  start((error, env) => {
+    if (error) {
+      throw error;
+    }
     console.log(`visit http://0.0.0.0:${env.PORT}/${apiVersion}/services`);
   });
+};
+
+// connect and start http server
+connect(error => {
+  if (error) {
+    throw error;
+  }
+  startHttpServer();
 });
